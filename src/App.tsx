242261612657abb294653876df5e9181aad88e06
Henrique Martins <hiikemartins@gmail.com>
2024-02-16 14:40:55 -0300

    Feat: Add images to folder public and creae component welcome and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import Welcome from "./components/welcome";
+import Header from "./components/header";
+
 const queryClient = new QueryClient();
 export default function App() {
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <Welcome />
+    </QueryClientProvider>
+  );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-black flex items-center justify-center py-5">
+      <img src="/logo.svg" width={180} />
+    </div>
+  );
+};
+
+export default Header;
